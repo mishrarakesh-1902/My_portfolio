70c3db4a7352b31b1db58d301df7099e9c88a890
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-07-31 22:58:12 +0530

    Fix: case-sensitive image path issue for deployment

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/myimage.jpg';
+import profileImage from '../../assets/myimage.png';
 
 const About = () => {
   return (
