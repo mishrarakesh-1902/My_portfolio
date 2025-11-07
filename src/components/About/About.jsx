afa8ff80ee0bbabb6b399b62382afab49ad2402a
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-11-07 17:40:41 +0530

    Add link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -94,7 +94,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1SjVWlQasxJamTHqV07LkSmvg0Rf86Xb7/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1qDPPixq8tNBZpCXPydgTar4eaPcL2guC/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
